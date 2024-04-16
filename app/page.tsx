feb48e1f829e5cfcd7cd7a745fc942bb1d17dbcb
Pierluigi <pierluigi.capirci89@gmail.com>
2024-04-16 20:07:15 +0100

    amend : added Navbar to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>
         Hello from the index page
       </h1>
