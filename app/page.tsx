fe4b73ba6db1ad3a6907f87fd3c85d548482cc37
Pierluigi <pierluigi.capirci89@gmail.com>
2024-04-16 20:43:38 +0100

    feat: added images with image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
-export default function Home() {
+import { Card } from "@/components/ui/card";
+import { simpleBlogCard } from "./lib/interface";
+import { client, urlFor } from "./lib/sanity";
+import { Image } from "next-sanity/image";
+
+async function getData() {
+  const query = `
+    *[_type == 'blog'] | order(_createdAt desc) {
+  title,
+    smallDescription,
+    "currentSlug" : slug.current,
+    titleImage
+}
+`;
+  const data = await client.fetch(query)
+  return data
+}
+
+
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData()
+  console.log(data)
   return (
-    <div>
-      <h1>
-        Hello from the index page
-      </h1>
-  </div>
+    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+      {data.map((post, index) => (
+        <Card key={index}>
+          <Image src={urlFor(post.titleImage).url()} alt="image" width={500} height={500} />
+        </Card>
+      ))}
+    </div>
   );
 }
