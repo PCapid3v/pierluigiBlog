053b6e338391bb86190f9b2244e402b6964a2f5a
Pierluigi <pierluigi.capirci89@gmail.com>
2024-04-18 10:19:19 +0100

    add : added revalidate to update cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
 import { Image } from "next-sanity/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const revalidate = 3600; // revalidate at most 1 hour
+
 
 async function getData() {
   const query = `
@@ -12,19 +17,34 @@ async function getData() {
     titleImage
 }
 `;
-  const data = await client.fetch(query)
-  return data
+  const data = await client.fetch(query);
+  return data;
 }
 
-
 export default async function Home() {
-  const data: simpleBlogCard[] = await getData()
-  console.log(data)
+  const data: simpleBlogCard[] = await getData();
+  console.log(data);
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+    <div className="grid grid-cols-1 lg:grid-cols-2 mt-5 gap-5">
       {data.map((post, index) => (
         <Card key={index}>
-          <Image src={urlFor(post.titleImage).url()} alt="image" width={500} height={500} />
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt="image"
+            width={500}
+            height={500}
+            className="rounded-t-lg h-[200px] object-cover "
+          />
+          <CardContent className="mt-5">
+            <h3 className="text-lg font-bold">{post.title}</h3>  
+            <p className="line-clamp-3 text-sm mt-3 text-gray-600 dark:text-gray-300">{post.smallDescription}</p>
+            <Button asChild className="w-full mt-7" >
+              <Link href={`/blog/${post.currentSlug}`} >
+                Read More
+              </Link>
+
+            </Button>
+          </CardContent>
         </Card>
       ))}
     </div>
