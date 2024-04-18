1547bad13c135248dfecc97a1aad07487bbc9105
Pierluigi Capirci <63102445+PCapid3v@users.noreply.github.com>
2024-04-18 10:32:37 +0100

    hide env variable

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
     apiVersion: "2023-05-03",
     dataset: "production",
-    projectId: "kv4qtme4",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     useCdn: false,
 });
 
